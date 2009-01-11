bd8ab7404784c086c6aaa723c0a37367d12b76b2
miya2000 <miya2000.opera@gmail.com>
2009-01-11 17:22:30 +0900

    fix XPath expression.

--- neko_mimi_fuxx.js
+++ neko_mimi_fuxx.js
@@ -15,7 +15,7 @@
 
 // http://d.hatena.ne.jp/amachang/20061201/1164986067
 /*@cc_on var currentScript = (function (e) { if(e.nodeName.toLowerCase() == 'script') return e; return arguments.callee(e.lastChild) })(document);
-// @*/ var currentScript = document.evaluate('//script[last()]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+// @*/ var currentScript = document.evaluate('/descendant::script[last()]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
 // http://twitter.com/nanto_vi/statuses/418740272
 var prog = currentScript.textContent || currentScript.text;
